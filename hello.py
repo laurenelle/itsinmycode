47c28c5000980de9ffff1433a438d946d5d962d5
laurenelle <lauren.elle.t@gmail.com>
2014-05-11 23:07:03 -0700

    Adds vintageTyping plugin

--- hello.py
+++ hello.py
@@ -1,8 +1,14 @@
 import os
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def hello_world():
+    return render_template("home.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/laturner/Projects/helloflask/venv"
+set -gx VIRTUAL_ENV "/Users/laturner/Projects/itsinmycode/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
